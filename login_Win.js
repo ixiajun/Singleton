e25fa8a3b221cf1cd364dfb832a8ca5345a9fd42
newbee <aixiajun@vip.qq.com>
2016-08-23 22:07:45 +0800

    首次提交

---
+++ login_Win.js
@@ -0,0 +1,39 @@
+'use strict';
+class LoginWinSingleton {
+  constructor() {
+    this.instance = null;
+    this.appendWin();
+    this.attachEvent();
+  }
+
+  static getInstance() {
+    if(!this.instance) {
+      this.instance = new LoginWinSingleton();
+    }
+    return this.instance;
+  }
+
+  appendWin() {
+    this.containerElement = document.createElement('div');
+    this.containerElement.className = 'layer';
+    this.containerElement.innerHTML = '我是登录窗口';
+    document.body.appendChild(this.containerElement);
+    this.containerElement.style.display = 'none';
+    // this.show();
+  }
+
+  attachEvent() {
+    var that = this;
+    this.containerElement.addEventListener('click', function () {
+      that.hide();
+    },false);
+  }
+
+  hide() {
+    this.containerElement.style.display = 'none';
+  }
+
+  show() {
+    this.containerElement.style.display = 'block';
+  }
+}
